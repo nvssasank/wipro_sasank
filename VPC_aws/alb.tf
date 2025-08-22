7af030c759c70daceda6c13a6a015af11250d7cc
Nimmana Venkata Sai Sasank <nvssasank1219@gmail.com>
2025-08-22 09:44:03 +0530

    Added ec2-terraform as a normal folder

--- VPC_aws/alb.tf
+++ VPC_aws/alb.tf
@@ -1,15 +1,15 @@
 resource "aws_lb" "my_alb" {
-  name               = "my-alb"
+  name               = "sasank-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
   subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
 
-  tags = { Name = "MyALB" }
+  tags = { Name = "sasankALB" }
 }
 
 resource "aws_lb_target_group" "my_tg" {
-  name     = "my-target-group"
+  name     = "sasank-target-group"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.my_vpc.id
